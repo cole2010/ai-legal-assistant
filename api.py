96bb0f515a52337cc03d10dbbc0339ac5e27d471
cole2010 <c_cassidy12@icloud.com>
2026-07-19 16:29:48 -0700

    Change Flask app port to 8080

--- api.py
+++ api.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,5 +10,4 @@ def ping():
     return "pong"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
